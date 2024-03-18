b7dd67088d1ec9684a37c4e6f63078d71bedf108
mostafa <mostafa.dekmak@enpal.de>
2024-03-19 00:51:48 +0100

    init

---
+++ infra/lib/constructs/s3CloudFrontStaticWebHosting.construct.ts
@@ -0,0 +1,64 @@
+import {Construct} from "constructs";
+import {BlockPublicAccess, Bucket} from "aws-cdk-lib/aws-s3";
+import {Duration, RemovalPolicy} from "aws-cdk-lib";
+import {AllowedMethods, Distribution, SecurityPolicyProtocol, ViewerProtocolPolicy} from "aws-cdk-lib/aws-cloudfront";
+import {S3Origin} from "aws-cdk-lib/aws-cloudfront-origins";
+
+interface IS3BucketConfig{
+    bucketId:string,
+    bucketName:string,
+}
+interface ICloudFrontDistribution{
+    cloudFrontId:string,
+}
+export interface IS3CloudFrontStaticWebHostingConstructProps{
+    s3BucketConfig:IS3BucketConfig,
+    cloudFrontDistribution:ICloudFrontDistribution
+}
+
+export class S3CloudFrontStaticWebHostingConstruct extends Construct{
+    constructor(scope: Construct, id: string,_props:IS3CloudFrontStaticWebHostingConstructProps) {
+        super(scope, id);
+
+
+        const bucket = this.createS3Bucket(_props.s3BucketConfig);
+        const cloudFrontDistribution :Distribution= this.createCloudFrontDistribution(_props.cloudFrontDistribution,bucket)
+
+
+    }
+    private createS3Bucket(_props:IS3BucketConfig){
+        const bucket : Bucket = new Bucket(this, _props.bucketId, {
+            bucketName:  _props.bucketName,
+            blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+            publicReadAccess: false,
+            removalPolicy: RemovalPolicy.DESTROY,
+        });
+
+        return bucket;
+    }
+    private createCloudFrontDistribution(_props:ICloudFrontDistribution,s3Bucket:Bucket){
+        const distribution = new Distribution(this, "TecoWebsiteDistribution", {
+            defaultBehavior: {
+                allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+                compress: true,
+                origin: new S3Origin(s3Bucket),
+                viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+            },
+            defaultRootObject: "index.html",
+            errorResponses: [
+                {
+                    httpStatus: 403,
+                    responseHttpStatus: 403,
+                    responsePagePath: "/error.html",
+                    ttl: Duration.minutes(30),
+                },
+            ],
+            minimumProtocolVersion: SecurityPolicyProtocol.TLS_V1_2_2019,
+        });
+
+        return distribution;
+    }
+
+
+
+}

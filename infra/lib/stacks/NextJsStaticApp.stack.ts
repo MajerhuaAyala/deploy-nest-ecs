f1b3920fa2461fa67c52b426004c2a646459d96b
mostafa <mostafa.dekmak@enpal.de>
2024-03-21 00:03:35 +0100

    init

--- infra/lib/stacks/NextJsStaticApp.stack.ts
+++ infra/lib/stacks/NextJsStaticApp.stack.ts
@@ -31,72 +31,8 @@ export class NextJsStaticAppStack extends Stack{
 
 
     private createS3BucketWithCloudFrontDistribution(_props:IS3CloudFrontStaticWebHostingConstructProps) {
-
         const s3BucketWithCFD :S3CloudFrontStaticWebHostingConstruct = new S3CloudFrontStaticWebHostingConstruct(this,"S3CloudFrontStaticWebHostingConstruct",_props);
         return s3BucketWithCFD;
-    }
-
-    private createCodePipeline(){
-
-        const outputSources = new codepipeline.Artifact();
-        const outputWebsite = new codepipeline.Artifact();
-
-        const pipeline : Pipeline = new Pipeline(this, 'MyFirstPipeline', {
-            pipelineName: 'MyPipeline',
-        });
-
-
-
-        pipeline.addStage({
-            stageName:"Source",
-            actions:[
-                new codepipeline_actions.GitHubSourceAction({
-                    actionName: 'GitHub_Source',
-                    owner: 'dkmostafa',
-                    repo: 'dev-samples',
-                    //@ts-ignore
-                    oauthToken: "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",
-                    output: outputSources,
-                    branch: 'develop', // default: 'master'
-                    trigger:codepipeline_actions.GitHubTrigger.WEBHOOK
-                })
-            ]
-        })
-
-        pipeline.addStage({
-            stageName: "Build",
-            actions: [
-                // AWS CodePipeline action to run CodeBuild project
-                new codepipeline_actions.CodeBuildAction({
-                    actionName: "Website",
-                    project: new codebuild.PipelineProject(this, "BuildWebsite", {
-                        projectName: "Website",
-                        buildSpec: codebuild.BuildSpec.fromSourceFilename(
-                            "./infra/buildspec.yml"
-                        ),
-                    }),
-                    input: outputSources,
-                    outputs: [outputWebsite],
-                }),
-            ],
-        });
-
-
-        //pipeline.addStage({
-        //   stageName: "Source",
-        //   actions: [
-        //     new CodePipelineAction.GitHubSourceAction({
-        //       actionName: "Checkout",
-        //       owner: props.github.owner,
-        //       repo: props.github.repository,
-        //       oauthToken: CDK.SecretValue.secretsManager("GitHubToken"),
-        //       output: outputSources,
-        //       trigger: CodePipelineAction.GitHubTrigger.WEBHOOK,
-        //     }),
-        //   ],
-        // });
-
-
     }
 
 

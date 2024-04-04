9416507fc3e96c41bc42e0cdf49ffe0941e243c7
mostafa <mostafa.dekmak@enpal.de>
2024-04-04 03:06:08 +0200

    Merge remote-tracking branch 'origin/nestjs-application' into nestjs-application

--- nestjs-app/src/app.service.ts
+++ nestjs-app/src/app.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): string {
     console.log("hello world from the app service")
-    return 'Hello World 13232234!';
+    return 'Hello World updated now!';
   }
 }
